b669a1e3021643c6eacf80f815c87035e9776cdf
fairct <fairct@gmail.com>
2013-06-24 10:22:46 -0400

    Fixed an issue left in with two cherry-picks.

--- src/com/android/settings/SecuritySettings.java
+++ src/com/android/settings/SecuritySettings.java
@@ -302,15 +302,6 @@ public class SecuritySettings extends SettingsPreferenceFragment
             }
         }
 
-        boolean isTelephony = pm.hasSystemFeature(PackageManager.FEATURE_TELEPHONY);
-        if (isTelephony) {
-            addPreferencesFromResource(R.xml.security_settings_app_cyanogenmod);
-            mSmsSecurityCheck = (ListPreference) root.findPreference(KEY_SMS_SECURITY_CHECK_PREF);
-            mSmsSecurityCheck.setOnPreferenceChangeListener(this);
-            int smsSecurityCheck = Integer.valueOf(mSmsSecurityCheck.getValue());
-            updateSmsSecuritySummary(smsSecurityCheck);
-         }
-
         return root;
     }
 
